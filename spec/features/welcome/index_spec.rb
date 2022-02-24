3483a0107200b4dba738f8a352c08106677ff168
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-02-23 22:39:47 -0700

    fix test test

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Landing Page' do
   it 'welcomes all visitors' do
     visit "/"
 
-    expect(page).to have_content "Welcome to The Garden Manager"
+    expect(page).to have_content "Welcome to The Garden Manager!"
   end
 end
