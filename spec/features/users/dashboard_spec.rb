691bf70543354dcd6cb0044b2ce96e0f1fa9ec95
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-02 12:22:50 -0600

    Adjust test to find weather section

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -2,103 +2,59 @@ require 'rails_helper'
 
 RSpec.describe 'Users Dashboard' do
   let(:user) { User.new({ id: '1', attributes: { name: "Raccoon22", email: "happy22@example.com", zip: "81007" } }) }
-  it 'has a seven day forecast' do
+
+  before(:each) do
+
     response = File.read('spec/fixtures/user.json')
     stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1')
       .to_return({
                    status: 200,
                    body: response
                  })
+
     response = File.read('spec/fixtures/forecast.json')
     stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/forecast')
       .to_return({
                    status: 200,
                    body: response
                  })
+
     response = File.read('spec/fixtures/frost_dates.json')
     stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/frostDates')
       .to_return({
                    status: 200,
                    body: response
                  })
+
     response = File.read('spec/fixtures/plants.json')
     stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/plants')
       .to_return({
                    status: 200,
                    body: response
                  })
+
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
+
     visit '/dashboard'
-    within '.weather' do
-      expect(page).to have_content('Weather Forecast')
+  end
+
+  it 'has a seven day forecast' do
+    within '#weather' do
       expect(page).to have_content('Low: 33.57° F')
       expect(page).to have_content('High: 56.16° F')
       expect(page).to have_content('Weather: Clear')
-    end
+    end 
   end
+
   it 'has a users frost dates and hardiness_zone' do
-    response = File.read('spec/fixtures/forecast.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/forecast')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/user.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/frost_dates.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/frostDates')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/plants.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/plants')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
-    visit '/dashboard'
     within '.frost-dates' do
       expect(page).to have_content('Fall frost date: May 31, temperature 36° F')
       expect(page).to have_content('Spring frost date: May 20, temperature 32° F')
     end
   end
-  it 'has a users plants' do
-    response = File.read('spec/fixtures/forecast.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/forecast')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/user.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/frost_dates.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/frostDates')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    response = File.read('spec/fixtures/plants.json')
-    stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/plants')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
-    allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
-    visit '/dashboard'
 
+  it 'has a users plants' do
     within '.plants' do
       expect(page).to have_content('My Plants!')
       expect(page).to have_content('Plant: Tomato')
