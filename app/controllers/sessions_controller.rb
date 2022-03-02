c1eeb15182e9d4391f34ff3fed0dded6c3978c8a
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-02 16:25:57 -0600

    Service Facade class method refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    facade = GardenFacade.new
     auth_hash = request.env['omniauth.auth'] #getting all info from google
     email = auth_hash['info']['email'] #grabbing email
     name = auth_hash['info']['name'] #grabbing name
-    user = facade.find_user_by_email(email)
+    user = GardenFacade.find_user_by_email(email)
     if user
       session[:user_id] = user.user_id
       session[:access_token] = auth_hash['credentials']['token']
       redirect_to "/dashboard", notice: "Welcome #{user.name}!"
     else
-      user = facade.create_user(email, name)
+      user = GardenFacade.create_user(email, name)
       session[:user_id] = user.user_id
       session[:access_token] = auth_hash['credentials']['token']
       redirect_to "/dashboard", notice: "Welcome #{user.name}!"
@@ -18,8 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # require "pry"; binding.pry
     session.destroy
-    redirect_to root_path, notice: "You have been successfully logged out!"
+    redirect_to root_path, notice: "You have been successfully logged out."
   end
 end
