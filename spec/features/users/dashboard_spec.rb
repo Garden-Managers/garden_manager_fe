3c984019ff9fb124b0a4c0ea6cfd217bc6094ae9
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-03 13:25:19 -0600

    Dashboard feature test adjusted for styling

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Dashboard' do
+
+  let(:user) { User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com', zip: '81007' } }) }
+  let(:user_2) { User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com' } }) }
+
+  before(:each) do
     response = File.read('spec/fixtures/user.json')
     stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1')
       .to_return({
@@ -44,24 +49,26 @@ RSpec.describe 'Users Dashboard' do
   end
 
   it 'has a users frost dates and hardiness_zone' do
-    within '.frost-dates' do
+    within '#frost-dates' do
       expect(page).to have_content('Fall frost date: May 31, temperature 36° F')
       expect(page).to have_content('Spring frost date: May 20, temperature 32° F')
     end
   end
 
   it 'has a users plants' do
-    within '.plants' do
-      expect(page).to have_content('My Plants!')
-      expect(page).to have_content('Plant: Tomato')
-      expect(page).to have_content('Frost Date: 7')
-      expect(page).to have_content('Maturity: 72')
-      expect(page).to have_content('Plant: Eggplant')
-      expect(page).to have_css('.plant-count', count: 40)
+    within '#plants' do
+      expect(page).to have_content('My Plants')
+      expect(page).to have_content('Tomato')
+      expect(page).to have_content('Safe planting time from frost date: 7')
+      expect(page).to have_content('Days to maturity: 72')
+      expect(page).to have_content('Eggplant')
+      expect(page).to have_content('40 plants in your collection')
     end
   end
   describe 'link to plants page' do
     it 'has a link to view plants' do
+      user = User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com', zip: '81007' } })
+      user_2 = User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com' } })
       response = File.read('spec/fixtures/forecast.json')
       stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/forecast')
         .to_return({
@@ -95,15 +102,15 @@ RSpec.describe 'Users Dashboard' do
       allow_any_instance_of(ApplicationController)
         .to receive(:current_user).and_return(user)
       visit '/dashboard'
-      within '.plants' do
-        click_link 'Explore Plants' do
-          expect(current_page).to eq(plants_path)
-        end
+      click_link 'Plant Library' do
+      expect(current_page).to eq(plants_path)
       end
     end
   end
   describe 'Zip form' do
     it 'has a form to add zip code if the user hasnt added one yet' do
+      user = User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com', zip: '81007' } })
+      user_2 = User.new({ id: '1', attributes: { name: 'Raccoon22', email: 'happy22@example.com' } })
       response = File.read('spec/fixtures/forecast.json')
       stub_request(:get, 'https://ancient-basin-82077.herokuapp.com/api/v1/users/1/forecast')
         .to_return({
@@ -140,16 +147,14 @@ RSpec.describe 'Users Dashboard' do
 
       visit '/dashboard'
 
-      within '.zip-form' do
+      within '#zip-form' do
         fill_in :zip, with: '80223'
 
         click_on 'Submit'
       end
       expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('Success! You can now see your local weather and planting data.')
 
-      within '.success' do
-        expect(page).to have_content('Zip has been added you can now get your forecast and frost dates!')
-      end
     end
   end
 end
