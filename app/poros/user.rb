702f1dadfafa6516200086a8d532fff055cf8d2a
Darren Kulback <dkulback@gmail.com>
2022-03-09 22:51:24 -0700

    user data nil upon login

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -2,10 +2,12 @@ class User
   attr_reader :user_id, :zip, :latitude, :longitude, :name
 
   def initialize(data)
-    @user_id = data[:id]
-    @name = data[:attributes][:name]
-    @zip = data[:attributes][:zip]
-    @latitude = data[:attributes][:latitude]
-    @longitude = data[:attributes][:longitude]
+    if data
+      @user_id = data[:id]
+      @name = data[:attributes][:name]
+      @zip = data[:attributes][:zip]
+      @latitude = data[:attributes][:latitude]
+      @longitude = data[:attributes][:longitude]
+    end
   end
 end
